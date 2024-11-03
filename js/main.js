27547c4d688810e4087c1dd0fe298f5ff5b2a2ac
mohammed elgasim <mohammedalgasim123@gmail.com>
2024-11-03 11:45:53 +0200

    update header and main banner

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+// Thumbs Up Animation
+function clicked(e) {
+  if (e.target.classList.contains("thumb-up-animation")) {
+    e.target.classList.remove("thumb-up-animation");
+  } else {
+    e.target.classList.add("thumb-up-animation");
+  }
+  let fill = e.target.getAttribute("fill");
+  if (fill == "blue") {
+    e.target.setAttribute("fill", "gray");
+  } else {
+    e.target.setAttribute("fill", "blue");
+  }
+}
+
 // ***********************************************************************
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -68,26 +83,3 @@ translateBtn.onclick = () => {
   }
 };
 /*************************************************************** */
-
-document.getElementById("submit").addEventListener("submit", function (event) {
-  event.preventDefault(); // منع إرسال النموذج تلقائيًا
-
-  // إعداد بيانات النموذج
-  const templateParams = {
-    name: document.getElementById("name").value,
-    email: document.getElementById("email").value,
-    phone: document.getElementById("phone").value,
-    location: document.getElementById("location").value,
-    feedback: document.getElementById("feedback").value,
-  };
-
-  // إرسال البريد باستخدام EmailJS
-  emailjs.send("service_d29d1gm", "template_j7onfw8", templateParams).then(
-    function (response) {
-      alert("Email sent successfully!");
-    },
-    function (error) {
-      alert("Failed to send email, please try again later.");
-    }
-  );
-});
